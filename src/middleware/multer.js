8ca5361ad0127db392c84bc3f0639a0831666b58
Serhii <suboxoneforhelp@gmail.com>
2026-03-06 02:50:04 +0200

    changed expected types of files for avatar

--- src/middleware/multer.js
+++ src/middleware/multer.js
@@ -8,19 +8,13 @@ export const upload = multer({
     fileSize: 2 * 1024 * 1024,
   },
   fileFilter: (req, file, cb) => {
-    const allowedTypes = [
-      'image/jpeg',
-      'image/jpg',
-      'image/png',
-      'image/gif',
-      'image/webp',
-    ];
-    if (allowedTypes.includes(file.mimetype)) {
+    const allowedType = 'image/';
+    if (file.mimetype === allowedType) {
       cb(null, true);
     } else {
       cb(
         new Error(
-          'Invalid file type. Only JPEG, PNG, GIF, and WebP are allowed.',
+          'Only images allowed',
         ),
         false,
       );
